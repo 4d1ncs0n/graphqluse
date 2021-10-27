76f746282eb722347d60d7467f59ce40bcf8a37b
Danny Velasquez <adincson@gmail.com>
2021-10-26 21:30:06 -0500

    feat: graphql config done on settings

--- graphqluse/settings.py
+++ graphqluse/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'django_graphiql', # -> For GUI interface
+    'graphene_django', # -> For graphql api
 ]
 
 MIDDLEWARE = [
