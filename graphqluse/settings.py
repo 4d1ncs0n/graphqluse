194628760e852a943fb29c12b4fdf706b2246259
Danny Velasquez <adincson@gmail.com>
2021-10-27 00:19:06 -0500

    fix: app name changed to products

--- graphqluse/settings.py
+++ graphqluse/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reviews',
-    'ecommerce',
+    'products',
     'django_graphiql', # -> For GUI interface
     'graphene_django', # -> For graphql api
 ]
