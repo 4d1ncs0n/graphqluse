f893056acce718b959303f01c5b338dc9396f1a9
Danny Velasquez <adincson@gmail.com>
2021-10-26 23:14:02 -0500

    feat: reviews app using graphql done

--- graphqluse/settings.py
+++ graphqluse/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'reviews',
     'django_graphiql', # -> For GUI interface
     'graphene_django', # -> For graphql api
 ]
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "reviews.schema.schema"
+}
