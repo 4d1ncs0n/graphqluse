2e246cf07161f75d790c63ba879925ec1e8eefec
Danny Velasquez <adincson@gmail.com>
2021-10-27 00:28:22 -0500

    fix: removed graphql endpoint

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import index
-from graphene_django.views import GraphQLView
 
 app_name = "reviews"
 
 urlpatterns = [
     path("", index, name="index"),
-    path("graphql", GraphQLView.as_view(graphiql=True)),
 ]
