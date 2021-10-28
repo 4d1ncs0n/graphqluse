c31a0e02e112233b30daa918e70b8acdabd8150f
Danny Velasquez <adincson@gmail.com>
2021-10-27 23:17:35 -0500

    feat: products access granted in urls

--- graphqluse/urls.py
+++ graphqluse/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf.urls import url
 from graphene_django.views import GraphQLView
 from products.schema import schema
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("reviews.urls", namespace="reviews")),
     path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('products', views.products, name='products'),
 ]
